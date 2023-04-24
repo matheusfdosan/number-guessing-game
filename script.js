833f6f7d3cbaf832b9e33cbccced364a2e659b03
Matheus Faustino <104006185+matheusfdosan@users.noreply.github.com>
2023-04-24 18:25:07 -0300

    :sparkles: feat: refresh page button

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const answer = Math.floor(Math.random() * 100) + 1
 let no_of_guesses = 0
 const guesses_nums = []
 
-document.querySelector(".my_btn").addEventListener("click", play)
+const btn = document.querySelector(".my_btn")
+
+btn.addEventListener("click", play)
 
 function play() {
   const user_guess = Number(document.querySelector(".guess").value)
@@ -15,22 +17,31 @@ function play() {
     alert("Please enter a number between 1 and 100.")
   } else {
     guesses_nums.push(user_guess)
-    no_of_guesses++
 
     if (user_guess < answer) {
+      ++no_of_guesses
       msg1.textContent = "Your guess is too low."
       msg2.textContent = "No. Of Guesses: " + no_of_guesses
       msg3.textContent = "Guessed numbers are: " + guesses_nums
     } else if (user_guess > answer) {
+      ++no_of_guesses
       msg1.textContent = "Your guess is too high."
       msg2.textContent = "No. Of Guesses: " + no_of_guesses
       msg3.textContent = "Guessed numbers are: " + guesses_nums
     } else if (user_guess === answer) {
       msg1.textContent = "You're goddamn right!"
       msg1.style.color = "#0f0"
+
       msg2.textContent = "The number was: " + answer
       msg3.textContent = "Guessed numbers it in " + no_of_guesses + " guesses."
-      document.querySelector('.my_btn').disabled = true
+
+      btn.textContent = "Replay"
+
+      btn.addEventListener("click", () => {
+        if (btn.textContent === "Replay") {
+          location.reload()
+        }
+      })
     }
   }
 }
